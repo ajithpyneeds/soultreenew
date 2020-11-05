169a411b0fe5631df7706384942136f1b316691d
ajithpyneeds <ajith.hlk@gmail.com>
2020-11-05 17:19:47 +0530

    specifications drop down

--- js/app.js
+++ js/app.js
@@ -282,3 +282,18 @@ function serenityFunc(evt,blocks) {
 document.getElementById(blocks).className = "planView tabcontent";
   evt.currentTarget.className += " active-spec";
 }
+
+// Specifications and Brands
+function specFunc() {
+  var btn = document.getElementById('btnFlex');
+  var specDescrip = document.getElementById('specsBrands');
+  if (specDescrip.style.display === 'none') {
+    specDescrip.style.display = 'block';
+    specDescrip.classList.add('animate__slideInRight');
+    specDescrip.classList.remove('animate__slideOutUp');
+  } else {
+    specDescrip.style.display = 'none';
+    specDescrip.classList.remove('animate__slideInRight');
+    specDescrip.classList.add('animate__slideOutUp');
+  }
+}
